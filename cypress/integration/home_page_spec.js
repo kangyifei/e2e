66d2c8e0bf50b59c72471d1886b0196d4c935712
kangyifei <530498334@qq.com>
2019-06-08 23:25:31 +0800

    init code

---
+++ cypress/integration/home_page_spec.js
@@ -0,0 +1,28 @@
+﻿describe('Todo App Test', function() {
+  it('successfully loads', function() {
+    cy.visit('/');
+    cy.get('input').type('Taks1');
+    cy.contains('Add').click();
+
+    cy.get('input').type('Taks2');
+    cy.contains('Add').click();
+
+    cy.get('input').type('Taks3');
+    cy.contains('Add').click();
+
+    cy.pause(1000);
+
+
+    cy.get('.delete-btn').first().click();
+
+    cy.contains('Edit').first().click();
+    cy.get('#app > div > table > tbody > tr:nth-child(1) > td:nth-child(1) > form > input').clear()
+        .type("changed1");
+    cy.get('button').contains('Save').click();
+
+    cy.pause(1000);
+
+    cy.contains('changed1').click();
+
+  })
+})
